f3876683a98693235ed8506599ba582786b6699c
sudha <sudhaks@users.noreply.github.com>
2022-01-12 15:12:25 +0530

    Update sorter.java

--- src/main/java/sorter.java
+++ src/main/java/sorter.java
@@ -5,7 +5,7 @@ public class sorter {
             int a[] = {6, 7, 1, 2, 4, 5, 3};
             System.out.println("input array = " + Arrays.toString(a));
             int temp;
-            for (int j = 0; j <= a.length / 2; j++) {
+            for (int j = 0; j < a.length - 1; j++) {
                 for (int i = 1; i < a.length; i++) {
 
                     if (a[i - 1] > a[i]) {
